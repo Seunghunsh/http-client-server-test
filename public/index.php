ba664801684032be7a5a94ab8c5a0cd165ba16a7
Lee_Seunghun <markandsh@gmail.com>
2016-07-11 14:04:05 -0400

    Update index.php by Lee

--- public/index.php
+++ public/index.php
@@ -1,7 +1,32 @@
-<?php
-require __DIR__ . '/../vendor/autoload.php';
 
 # TIP: Use the $_SERVER Sugerglobal to get all the data your need from the Client's HTTP Request.
 
 # TIP: HTTP headers are printed natively in PHP by invoking header().
 #      Ex. header('Content-Type', 'text/html');
+
+# my approach
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php  
+ ob_start();  //begin buffering the output  
+?> 
+Welcome to my website!
+<?php 
+  if(true){ 
+   echo "You're in!"; 
+  } 
+  else{ 
+header('Content-Type', 'text/html');
+  } 
+require __DIR__ . '/../vendor/autoload.php’; 
+foreach($_SERVER as $name=>$value) {
+    echo “<li>$name = $value</li>\n”;
+}
+  ob_flush(); //output the data in the buffer 
+?>
+
+</body>
+</html>
